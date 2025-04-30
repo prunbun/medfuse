45434864b0fce9daacf30437337e4f535f885209
Pranav Thatte <thattepranav@gmail.com>
2025-04-30 13:29:20 -0500

    sync

--- scripts/train_xray_encoder.sh
+++ scripts/train_xray_encoder.sh
@@ -3,4 +3,4 @@ CUDA_VISIBLE_DEVICES=0 CUDA_LAUNCH_BLOCKING=1 python scripts/fusion_main.py --di
 --epochs 100 --pretrained \
 --vision-backbone resnet34 --data_pairs radiology \
 --batch_size 16 --align 0.0 --labels_set radiology --save_dir /content/drive/MyDrive/medfuse_checkpoints/cxr_encoder_p10_ehrsplit \
---fusion_type uni_cxr --layers 2 --vision_num_classes 14 \
+--fusion_type uni_cxr --layers 2 --vision_num_classes 14
