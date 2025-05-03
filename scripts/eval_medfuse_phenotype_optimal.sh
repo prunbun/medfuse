8188899114de90ba797059bbb65cae212934e57f
Pranav Thatte <thattepranav@gmail.com>
2025-05-03 14:06:38 -0500

    sync

--- scripts/eval_medfuse_phenotype_optimal.sh
+++ scripts/eval_medfuse_phenotype_optimal.sh
@@ -6,5 +6,5 @@ CUDA_VISIBLE_DEVICES=0 CUDA_LAUNCH_BLOCKING=1 python scripts/fusion_main.py \
 --vision_num_classes 14 --num_classes 25 \
 --data_pairs partial_ehr_cxr \
 --fusion_type lstm \
---save_dir /content/drive/MyDrive/medfuse_checkpoints/fusion_partial/baseline/eval \
---load_state /content/drive/MyDrive/medfuse_checkpoints/their_best_checkpoints/pretrained_encoders/best_checkpoint.pth.tar
+--save_dir /content/drive/MyDrive/medfuse_checkpoints/fusion_partial/baseline/eval_optimal \
+--load_state /content/drive/MyDrive/medfuse_checkpoints/their_best_checkpoints/optimal/best_checkpoint.pth.tar
