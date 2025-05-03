b1e009340271f4735c9d4ab8f181fece7ec75366
Pranav Thatte <thattepranav@gmail.com>
2025-05-03 14:37:16 -0500

    sync

---
+++ scripts/eval_medfuse_module_mortality_reversed.sh
@@ -0,0 +1,13 @@
+CUDA_VISIBLE_DEVICES=0 CUDA_LAUNCH_BLOCKING=1 python scripts/fusion_main.py \
+--dim 256 --dropout 0.3 --layers 2 \
+--vision-backbone resnet34 \
+--mode eval \
+--epochs 5 --batch_size 16 \
+--vision_num_classes 14 --num_classes 1 \
+--data_pairs partial_ehr_cxr \
+--data_ratio 1.0 \
+--task in-hospital-mortality \
+--labels_set mortality \
+--fusion_type lstm \
+--save_dir /content/drive/MyDrive/medfuse_checkpoints/fusion_partial/mortality_reversed/eval \
+--load_state /content/drive/MyDrive/medfuse_checkpoints/fusion_partial/mortality_reversed/best_checkpoint.pth.tar
