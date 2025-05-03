a3267d41779d8e5da4a16113e90aef0d7152bfb7
Pranav Thatte <thattepranav@gmail.com>
2025-05-03 14:20:49 -0500

    sync

--- scripts/eval_medfuse_phenotype_final.sh
+++ scripts/eval_medfuse_phenotype_final.sh
@@ -2,7 +2,7 @@ CUDA_VISIBLE_DEVICES=0 CUDA_LAUNCH_BLOCKING=1 python scripts/fusion_main.py \
 --dim 256 --dropout 0.3 --layers 2 \
 --vision-backbone resnet34 \
 --mode eval \
---epochs 5 --batch_size 16 \
+--epochs 50 --batch_size 16 \
 --vision_num_classes 14 --num_classes 25 \
 --data_pairs partial_ehr_cxr \
 --fusion_type lstm \
